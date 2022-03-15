c9b5292df6229b5aa03a68fa8b6c850a28b028f9
gabyse1 <gaby.es.iaa@gmail.com>
2022-03-15 06:38:40 -0500

    Update ItemDetailsPollution component to display the air pollution data retrieved from the store

--- src/components/ItemDetailsPollution.js
+++ src/components/ItemDetailsPollution.js
@@ -1,29 +1,65 @@
 import { PropTypes } from 'prop-types';
 
-const ItemDetailsPollution = ({ detailsPollution }) => (
-  <section className="page__section">
-    <div className="components__container">
-      <div className="component__item">
-        <div className="item__quantity">
-          <span>CO</span>
-          <span>{`${detailsPollution.components.co} μg/m3`}</span>
-        </div>
-        <div className="item__color-aqi">
-          <div className="aqi__circle">
-            {detailsPollution.components.co}
-          </div>
-        </div>
+const ItemDetailsPollution = ({ detailsPollution }) => {
+  const chArr = Object.entries(detailsPollution.list[0].components);
+
+  return (
+    <section className="page__section">
+      <div className="components__container">
+        {
+          chArr.map((obj) => (
+            <div className="component__item" key={`${obj[0]}-${obj[1]}`}>
+              <div className="item__quantity">
+                <span>{obj[0]}</span>
+                <span>{`${obj[1]} μg/m3`}</span>
+              </div>
+              <div className="item__color-aqi">
+                <div className="aqi__circle">
+                  {obj[1]}
+                </div>
+              </div>
+            </div>
+          ))
+        }
       </div>
-    </div>
-  </section>
-);
+    </section>
+  );
+};
 
 ItemDetailsPollution.propTypes = {
   detailsPollution: PropTypes.shape({
-    main: PropTypes.objectOf(PropTypes.number).isRequired,
-    components: PropTypes.objectOf(PropTypes.number).isRequired,
-    dt: PropTypes.number.isRequired,
+    coord: PropTypes.objectOf(PropTypes.number).isRequired,
+    list: PropTypes.arrayOf(PropTypes.shape({
+      main: PropTypes.objectOf(PropTypes.number).isRequired,
+      components: PropTypes.objectOf(PropTypes.number).isRequired,
+      dt: PropTypes.number.isRequired,
+    })).isRequired,
   }).isRequired,
 };
+
+// {
+//   "coord": {
+//       "lon": 50,
+//       "lat": 50
+//   },
+//   "list": [
+//       {
+//           "main": {
+//               "aqi": 2
+//           },
+//           "components": {
+//               "co": 273.71,
+//               "no": 0,
+//               "no2": 0.56,
+//               "o3": 113.01,
+//               "so2": 1.88,
+//               "pm2_5": 10.66,
+//               "pm10": 10.67,
+//               "nh3": 0.24
+//           },
+//           "dt": 1647280800
+//       }
+//   ]
+// }
 
 export default ItemDetailsPollution;
