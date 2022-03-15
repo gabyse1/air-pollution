f17238e008dd634c4c09fd1c22a09b5ae794e015
gabyse1 <gaby.es.iaa@gmail.com>
2022-03-15 06:26:23 -0500

    Create cities reducer to search and get cities from API and update the store

---
+++ src/redux/city/citiesReducer.js
@@ -0,0 +1,39 @@
+const baseURL = 'http://api.openweathermap.org/geo/1.0/direct?';
+const apiKey = '382d7d126d3c48b5414498da95bcffaf';
+
+const GET_SEARCHED_CITIES_FROM_API = 'cityStore/citY/GET_SEARCHED_CITIES_FROM_API';
+
+const initializeState = [];
+
+const getSearchedCitiesAPI = (nameCity) => async (dispatch) => {
+  await fetch(`${baseURL}q=${nameCity}&limit=5&appid=${apiKey}`)
+    .then((response) => response.json())
+    .then((data) => {
+      const newInitializeState = data.map((obj) => ({
+        name: obj.name,
+        lat: obj.lat,
+        lon: obj.lon,
+        country: obj.country,
+        state: obj.state,
+      }));
+      dispatch({
+        type: GET_SEARCHED_CITIES_FROM_API,
+        payload: newInitializeState,
+      });
+    });
+};
+
+const citiesReducer = (state = initializeState, action) => {
+  switch (action.type) {
+    case GET_SEARCHED_CITIES_FROM_API:
+      return action.payload;
+    default:
+      return state;
+  }
+};
+
+export {
+  getSearchedCitiesAPI,
+};
+
+export default citiesReducer;
