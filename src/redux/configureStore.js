d6e3c7c308a763accaecac0c3a3aa5825b957f21
gabyse1 <gaby.es.iaa@gmail.com>
2022-03-18 15:15:52 -0500

    Delete pollution reducer

--- src/redux/configureStore.js
+++ src/redux/configureStore.js
@@ -3,12 +3,10 @@ import { composeWithDevTools } from '@redux-devtools/extension';
 import logger from 'redux-logger';
 import thunk from 'redux-thunk';
 import citiesReducer from './cities/citiesReducer';
-import pollutionReducer from './pollution/pollutionReducer';
 import forecastReducer from './forecast/forecastReducer';
 
 const rootReducer = combineReducers({
   citiesReducer,
-  pollutionReducer,
   forecastReducer,
 });
 
